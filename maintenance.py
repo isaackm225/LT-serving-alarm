6894339486e7cbe1cec0e38965839de8a9d42cc1
Isaac K <95644163+Izy-stack@users.noreply.github.com>
2022-10-27 03:04:24 -0400

    Update maintenance.py

--- maintenance.py
+++ maintenance.py
@@ -4,3 +4,8 @@ class logwriter():
     def write_log(self):        
         with open("log.txt","a+") as log:
             log.write(f"The alarm ran on {datetime.now()}")
+        
+    def log_success(self):
+        with open("log.txt","a+") as log:
+            log.write(f"Successfully solved leetcode problem {datetime.now()}")
+        
